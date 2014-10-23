020760e820e46f3941e5740d885b4a5c934f066c
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-10-23 12:18:49 -0500

    implement basic CRUD with Patient Model

--- hospital/patient/views.py
+++ hospital/patient/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render_to_response
 from rest_framework import viewsets
 from rest_framework import permissions
 from patient.permissions import IsSuperuserOrReadOnly
@@ -5,7 +6,12 @@ from .serializers import PatientSerializer
 from .models import Patient
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView
+from django.views.generic import DeleteView
+from django.template import RequestContext
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.http import HttpResponseRedirect, HttpResponseNotAllowed
+from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -49,3 +55,103 @@ class PatientListView(LoginRequiredMixin, ListView):
 class PatientDetailView(LoginRequiredMixin, DetailView):
     model = Patient
     template_name = 'patient/detail.html'
+
+
+# solo como superuser
+class PatientDeleteView(LoginRequiredMixin, DeleteView):
+    model = Patient
+    success_url = reverse_lazy('patient_app:patients-list')
+    template_name = 'patient/delete.html'
+
+
+def record_patient(request):
+    dni = request.POST['dni']
+    superuser = request.user
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    age = request.POST['age']
+    sex = request.POST['sex']
+    address = request.POST['address']
+    email = request.POST['email']
+    telephone = request.POST['telephone']
+    cellphone = request.POST['cellphone']
+
+    patient = Patient.objects.create(
+        dni=dni,
+        superuser=superuser,
+        first_name=first_name,
+        last_name=last_name,
+        age=age,
+        sex=sex,
+        address=address,
+        email=email,
+        telephone=telephone,
+        cellphone=cellphone,
+    )
+
+    return patient
+
+
+def patient_create_view(request):
+    if request.user.is_authenticated() and request.user.is_superuser:
+        if request.method == "POST":
+            patient = record_patient(request)
+            if patient:
+                return HttpResponseRedirect(
+                    reverse('patient_app:patients-list')
+                )
+
+        return render_to_response(
+            'patient/create.html',
+            context_instance=RequestContext(request),
+        )
+
+    return HttpResponseNotAllowed(['GET'])
+
+
+def update_patient(request):
+    dni = request.POST['dni']
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    age = request.POST['age']
+    sex = request.POST['sex']
+    address = request.POST['address']
+    email = request.POST['email']
+    telephone = request.POST['telephone']
+    cellphone = request.POST['cellphone']
+
+    patient = get_object_or_404(Patient, pk=dni)
+    patient.dni = dni
+    patient.first_name = first_name
+    patient.last_name = last_name
+    patient.age = age
+    patient.sex = sex
+    patient.address = address
+    patient.email = email
+    patient.telephone = telephone
+    patient.cellphone = cellphone
+    patient.save()
+
+    return patient
+
+
+def patient_update_view(request, dni):
+    if request.user.is_authenticated() and request.user.is_superuser:
+        if request.method == "POST":
+            patient = update_patient(request)
+            if patient:
+                return HttpResponseRedirect(
+                    reverse('patient_app:patients-list')
+                )
+
+        patient = get_object_or_404(Patient, pk=dni)
+
+        ctx = {'patient': patient}
+
+        return render_to_response(
+            'patient/update.html',
+            ctx,
+            context_instance=RequestContext(request),
+        )
+
+    return HttpResponseNotAllowed(['GET'])
